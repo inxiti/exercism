de38efaaea17bd571db0a7ee5f72eac74db4188a
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-11 16:59:38 -0500

    updated hamming solution to use zip

--- swift/Hamming.swift
+++ swift/Hamming.swift
@@ -1,9 +1,7 @@
 class Hamming {
     class func compute(_ a: String, against b: String) -> Int? {
         guard a.count == b.count else { return nil }
-
-        return a.enumerated().flatMap {
-            return b[b.index(b.startIndex, offsetBy: $0)] != $1 ? 1 : nil
-        }.count
+                
+        return zip(a, b).filter { $0 != $1 }.count
     }
 }
