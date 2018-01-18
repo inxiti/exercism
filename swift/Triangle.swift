c1754d06bb35aa35df82851cc973af892c74b004
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-18 00:03:59 -0500

    updated triangle solution formatting

--- swift/Triangle.swift
+++ swift/Triangle.swift
@@ -1,22 +1,14 @@
 class Triangle {
     let kind: String
-    let triangleKind = (
-        Equilateral:    "Equilateral",
-        Isosceles:      "Isosceles",
-        Scalene:        "Scalene",
-        ErrorKind:      "ErrorKind"
-    )
-    
+
     init(_ a: Double, _ b: Double, _ c: Double) {
-        guard (a > 0 && b > 0 && c > 0) && (a <= b + c && b <= a + c &&  c <=  a + b) else {
-            kind = triangleKind.ErrorKind
-            
+        guard a > 0 && b > 0 && c > 0 && a <= b + c && b <= a + c &&  c <=  a + b else {
+            kind = "ErrorKind"
             return
         }
         
-        if a == b && b == c { kind = triangleKind.Equilateral }
-        else if a == b || a == c || b == c { kind = triangleKind.Isosceles }
-        else { kind = triangleKind.Scalene }
+        if a == b && b == c { kind = "Equilateral" }
+        else if a == b || a == c || b == c { kind = "Isosceles" }
+        else { kind = "Scalene" }
     }
 }
-
