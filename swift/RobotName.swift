8380b5efe1a8570ad54b5bd6bf60852d0872fd5c
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-19 00:15:00 -0500

    updated robot name solution, added robot name registry to track used names

--- swift/RobotName.swift
+++ swift/RobotName.swift
@@ -2,15 +2,19 @@ import Foundation
 
 class Robot {
     private var _name: String = ""
+    private var _names: Set<String> = []
     
     var name: String {
         get {
             if _name.isEmpty {
                 let letters = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-                let index = Int(arc4random_uniform(UInt32(letters.count - 2)))
-                let number = arc4random_uniform(899) + 100
                 
-                _name = "\(letters[index])\(letters[index + 1])\(number)"
+                repeat {
+                    let index = Int(arc4random_uniform(24))
+                    let number = arc4random_uniform(899) + 100
+                    
+                    _name = "\(letters[index])\(letters[index + 1])\(number)"
+                } while _names.contains(_name)
             }
             
             return _name
