0e8496e7b9e9c45c607e8b73bd489a36cded61b9
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-23 16:36:44 -0500

    updated sublist solution

--- swift/Sublist.swift
+++ swift/Sublist.swift
@@ -3,15 +3,12 @@ import Foundation
 enum Sublist { case equal, sublist, superlist, unequal }
 
 func classifier(listOne l1: [Int], listTwo l2: [Int]) -> Sublist {
-    if l1 == l2 { return .equal }
-    if l1.isEmpty && !l2.isEmpty { return .sublist }
-    if !l1.isEmpty && l2.isEmpty { return .superlist }
-    
     let s1 = String(describing: l1).dropFirst().dropLast()
     let s2 = String(describing: l2).dropFirst().dropLast()
     
-    if s1.contains(s2) { return .superlist }
-    if s2.contains(s1) { return .sublist }
+    if l1 == l2 { return .equal }
+    if s1.contains(s2) || s2.isEmpty { return .superlist }
+    if s2.contains(s1) || s1.isEmpty { return .sublist }
     
     return .unequal
 }
