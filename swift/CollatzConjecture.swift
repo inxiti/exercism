45c7ad57e493395387cb6b49bca5e0d2095c3abd
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-24 10:56:25 -0500

    updated collatz conjecture solution

--- swift/CollatzConjecture.swift
+++ swift/CollatzConjecture.swift
@@ -3,15 +3,8 @@ enum NumberError: Error { case invalid }
 class CollatzConjecture {
     class func steps(_ n: Int) throws -> Int {
         guard n > 0 else { throw NumberError.invalid }
-        
-        var counts: [Int] = [1]
-        
-        func collatzSequenceCount(of n: Int) -> Int {
-            guard counts.count < n else { return counts[n - 1] }
+        guard n > 1 else { return 0 }
 
-            return 1 + collatzSequenceCount(of: n % 2 == 0 ? n / 2 : 3 * n + 1)
-        }
-        
-        return collatzSequenceCount(of: n) - 1
+        return try 1 + steps(n % 2 == 0 ? n / 2 : 3 * n + 1)
     }
 }
