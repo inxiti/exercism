cde74db965b9319f7f85c6c0f449b2aad4effcab
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-25 17:50:09 -0500

    updated isbn verifier solution

--- swift/IsbnVerifier.swift
+++ swift/IsbnVerifier.swift
@@ -1,5 +1,5 @@
 func isValid(_ i: String) -> Bool {
-    let isbn = i.filter { "0123456789X".contains($0) }.flatMap { $0 == "X" ? 10 : Int(String($0)) }
+    let isbn = i.filter { "0123456789X".contains($0) }.map { Int(String($0)) ?? 10 }
     
     guard isbn.count == 10 && !isbn.prefix(9).contains(10) else { return false }
 
