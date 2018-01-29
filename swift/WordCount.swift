cd4c8b47c98bad84f909a94d43ce811cd1279060
inxiti <32788205+inxiti@users.noreply.github.com>
2018-01-29 16:45:22 -0500

    added word count solution

---
+++ swift/WordCount.swift
@@ -0,0 +1,16 @@
+class WordCount {
+    let words: [String]
+    
+    init(words w: String) {
+        words = w
+            .lowercased()
+            .filter { "abcdefghijklmnopqrstuvwxyz1234567890 ".contains($0) }
+            .split(separator: " ")
+            .flatMap { String($0) }
+    }
+    
+    func count() -> Dictionary<String, Int> {
+        return words
+            .reduce(into: [String: Int]()) { $0[String($1)] = ($0[String($1)] ?? 0) + 1 }
+    }
+}
