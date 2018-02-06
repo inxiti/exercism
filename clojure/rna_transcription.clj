fecc97ffa029b183bac788304320f9d809082570
inxiti <32788205+inxiti@users.noreply.github.com>
2018-02-05 22:48:19 -0500

    added rna transcription solution

---
+++ clojure/rna_transcription.clj
@@ -0,0 +1,19 @@
+(ns rna-transcription
+  (:require [clojure.string :as string]))
+
+(defn convert
+  "Transcribe DNA nucleotide to RNA complement."
+  [n]
+  (case n
+    \G "C"
+    \C "G"
+    \T "A"
+    \A "U"
+    (throw (AssertionError.))))
+
+(defn to-rna
+  "Given a DNA strand, return its RNA complement."
+  [[first & rest]]
+  (if (some? rest)
+    (str (convert first) (to-rna rest))
+    (convert first)))
