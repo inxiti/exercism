88e5cae538156294ec6a5851a58f951aa5cc2275
inxiti <32788205+inxiti@users.noreply.github.com>
2018-02-07 16:52:47 -0500

    added isbn verifier solution

---
+++ clojure/isbn_verifier.clj
@@ -0,0 +1,14 @@
+(ns isbn-verifier
+  (:require [clojure.string :as string]))
+
+(defn isbn?
+  "Check if a given ISBN-10 is valid."
+  [isbn]
+  (if (string/includes? (subs (string/reverse isbn) 1) "X")
+    false
+    (let [lst     (string/split isbn #"")
+          valid   (filter #(string/includes? "0123456789X" (str %)) lst)
+          isbn'   (map #(Integer/parseInt %) (replace {"X" "10"} valid))
+          r       (range 0 (count isbn'))
+          result  (for [i r] (* (nth isbn' i) (- 10 i)))]
+      (== (mod (reduce + result) 11) 0))))
