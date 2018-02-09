88c002b6c9db475f0120bc1f9e831507e00e0ea2
inxiti <32788205+inxiti@users.noreply.github.com>
2018-02-08 19:51:27 -0500

    added word count solution

---
+++ clojure/word_count.clj
@@ -0,0 +1,12 @@
+(ns word-count
+  (:require [clojure.string :as string]))
+
+(defn word-count
+  "Count the occurences of each word in string."
+  [w]
+  (-> w
+      (string/lower-case)
+      (string/replace #"[^a-zA-Z0-9 ]" "")
+      (string/split #" ")
+      (#(remove string/blank? %))
+      (frequencies)))
